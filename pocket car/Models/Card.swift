0d9bb52db1a9034e7c177abe6e3005f42f3eb9c1
fkyle742446 <cailles.florian@gmail.com>
2025-03-30 18:21:29 +0200

    [autosave]

--- pocket car/Models/Card.swift
+++ pocket car/Models/Card.swift
@@ -1,33 +1,9 @@
 struct Card {
     let name: String
-    let rarity: CardRarity
+    let rarity: Rarity
 }
 
-enum CardRarity: String, CaseIterable {
-    case common = "Common"
-    case rare = "Rare"
-    case epic = "Epic"
-    case legendary = "Legendary"
-    
-    var sortOrder: Int {
-        switch self {
-        case .common: return 1
-        case .rare: return 2
-        case .epic: return 3
-        case .legendary: return 4
-        }
-    }
-    
-    var color: Color {
-        switch self {
-        case .common:
-            return .gray
-        case .rare:
-            return .blue
-        case .epic:
-            return .purple
-        case .legendary:
-            return .orange
-        }
-    }
+enum Rarity {
+    case common, rare, legendary
 }
+
